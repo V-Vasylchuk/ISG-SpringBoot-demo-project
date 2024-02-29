2638a8f2fe11675396425bcafab527925c724a54
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2024-02-29 17:54:56 +0200

    initial commit

---
+++ src/main/java/com/vvs/demoProject/IsgSpringBootDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vvs.demoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsgSpringBootDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsgSpringBootDemoProjectApplication.class, args);
+    }
+
+}
