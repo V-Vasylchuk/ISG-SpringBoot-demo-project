56b3c638f3fe3bf0ed17339734605ff761076860
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2024-03-05 12:40:26 +0200

    add docker & liquibase migration

---
+++ src/main/resources/changelog/changes/create-cars-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS cars (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    model VARCHAR(255) NOT NULL,
+    brand VARCHAR(255) NOT NULL,
+    `type` VARCHAR(255) NOT NULL,
+    inventory INT NOT NULL,
+    daily_fee DECIMAL(10, 2) NOT NULL,
+    deleted TINYINT(1) NOT NULL DEFAULT 0
+);
